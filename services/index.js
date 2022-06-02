13bbc8558c56c0bc25a97d0cbf75a3acf3db7d5c
unknown <setprogramming@gmail.com>
2022-06-02 10:24:08 +0300

    finished the PostDetail component - damn complicated...

--- services/index.js
+++ services/index.js
@@ -37,6 +37,40 @@ export const getPosts = async () => {
     return result.postsConnection.edges
 }
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+      query GetPostDetails($slug: String!) {
+          post(where: {slug: $slug}) {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+                url
+            }
+            categories {
+              name
+              slug
+            }
+            content {
+              raw
+            }
+          }
+        }            
+  `
+  const result = await request(graphqlAPI, query, {slug})
+
+  return result.post
+}
+
 export const getRecentPosts = async () => {
     const query = gql`
       query GetPostDetails() {
@@ -58,7 +92,7 @@ export const getRecentPosts = async () => {
     return result.posts
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
   query GetPostDetails($slug: String!, $categories: [String!]) {
     posts(
@@ -81,7 +115,7 @@ export const getSimilarPosts = async () => {
     }
   }
   `
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query, {categories, slug})
 
   return result.posts
 }
